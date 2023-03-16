b6e2eed97f891ed1a6755af587fb52da61446cf4
DESKTOP-IOTIHVV\henry zambrano <hezampo@gmail.com>
2023-03-16 11:25:05 -0500

    cambio en el llamado a las rutas

--- src/main/java/com/example/siontravel/Controller/HomeController.java
+++ src/main/java/com/example/siontravel/Controller/HomeController.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.example.siontravel.Model.Services.RutasInterfacesServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
@@ -33,6 +34,9 @@ public class HomeController {
 
     @Autowired
     private AreasInterfacesServices areasInterfacesServices;
+
+    @Autowired
+    private RutasInterfacesServices rutasInterfacesServices;
     
     @Autowired
     private ReservasInterfacesServices reservasInterfacesServices;
@@ -43,8 +47,9 @@ public class HomeController {
         Reservas reservas = new Reservas();
         model.addAttribute("areasLista", listaAreas);
         model.addAttribute("reservas", reservas);
+        model.addAttribute("flag", true);
         model.addAttribute("titulo", "Formulario de Booking");
-        return "home";
+        return "/home";
     }
 
     @RequestMapping("/login")
@@ -56,10 +61,10 @@ public class HomeController {
     public String booking(@ModelAttribute Reservas reservas,Model model) {
         model.addAttribute("titulo", "Lista de rutas para su fecha de viaje");
         model.addAttribute("reservas", reservas);
-        List<?> listaFrom = onWayTrip(reservas.getAreas_from().getId(), reservas.getAreas_to().getId(), reservas.getFecha_salida());
+        List<?> listaFrom = rutasInterfacesServices.onWayTrip(reservas.getAreas_from().getId(), reservas.getAreas_to().getId(), reservas.getFecha_salida());
         model.addAttribute("onewaytrip", listaFrom);
         if(Constantes.ROUND_TRIP.equals(reservas.getTipo_reserva())) {
-        	List<?> listaTo = roundTrip(reservas.getAreas_from().getId(), reservas.getAreas_to().getId(), reservas.getFehca_llegada());
+        	List<?> listaTo = rutasInterfacesServices.roundTrip(reservas.getAreas_from().getId(), reservas.getAreas_to().getId(), reservas.getFehca_llegada());
         	model.addAttribute("roundtrip", listaTo);
         }
         return "/booking";
@@ -73,7 +78,6 @@ public class HomeController {
         reservas.setNumero_reserva(numero_reserva);
         model.addAttribute("reservas", reservas);
         model.addAttribute("listaPrecios", listaPrecios);
-        
         return "/confirm";
     }
     
@@ -125,20 +129,8 @@ public class HomeController {
         reservasInterfacesServices.guardar(reservas);
         return "redirect:/";
     }
-    
-    public List<?> onWayTrip(int id_from, int id_to, String fecha_salida){
-        var sql = "SELECT DISTINCT r.id AS id_ruta, r.precio_adulto, r.precio_infante, r.precio_equipaje, a.nombre AS origen, a1.nombre AS destino, b.hora_salida, b.hora_llegada, b.id AS id_bus, b.bus, b.id_from, b1.id_to FROM rutas r LEFT JOIN area a ON (r.id_from = a.id) LEFT JOIN area a1 ON (r.id_to = a1.id) LEFT JOIN bus b ON (a.id = b.id_from) LEFT JOIN bus b1 ON (a1.id = b1.id_to) WHERE r.id_from = "+id_from+" AND r.id_to = "+id_to+" AND b.id_from = "+id_from+" AND b.id_to = "+id_to+" AND r.fecha_inicio <= '"+fecha_salida+"' AND r.fecha_final >= '"+fecha_salida+"' ";
-        onwaytrip = this.jdbcTemplate.queryForList(sql);
-		return onwaytrip;
-    }
 
-    public List<?> roundTrip(int id_from, int id_to, String fecha_llegada){
-        var sql = "SELECT DISTINCT r.id AS id_ruta, r.precio_adulto, r.precio_infante, r.precio_equipaje, a.nombre AS origen, a1.nombre AS destino, b.hora_salida, b.hora_llegada, b.id AS id_bus, b.bus, b.id_from, b1.id_to FROM rutas r LEFT JOIN area a ON (r.id_from = a.id) LEFT JOIN area a1 ON (r.id_to = a1.id) LEFT JOIN bus b ON (a.id = b.id_from) LEFT JOIN bus b1 ON (a1.id = b1.id_to) WHERE r.id_to = "+id_from+" AND r.id_from = "+id_to+" AND b.id_to = "+id_from+" AND b.id_from = "+id_to+" AND r.fecha_inicio <= '"+fecha_llegada+"' AND r.fecha_final >= '"+fecha_llegada+"' ";
-        roundtrip = this.jdbcTemplate.queryForList(sql);
-        return roundtrip;
-    }
-
-    @GetMapping("/admin/inicio")
+    @GetMapping({"/admin/inicio","/admin"})
     public String indexAdmin(){
         return "redirect:/admin/areas/";
     }
